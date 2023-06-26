acb966a25ba156ff16b553ca6b8fddd1912d0d2c
Robert Cato <saiwolf@swmnu.net>
2023-06-26 13:06:06 -0500

    Added comments to tests to explain steps.

--- RPN.Calc.Tests/Tests.cs
+++ RPN.Calc.Tests/Tests.cs
@@ -8,7 +8,7 @@ public class Tests
     public void BasicNotationParsing()
     {
         using RPN rpn = new();
-        rpn.Parse("5 2 + -3 - 10 +");
+        rpn.Parse("5 2 + -3 - 10 +"); // (5+2) - (-3) + 10 = 20
         Assert.That(rpn.Peek(), Is.EqualTo("20"));
     }
 
@@ -16,11 +16,11 @@ public class Tests
     public void ManualOperations()
     {
         using RPN rpn = new();
-        rpn.Push("10");
+        rpn.Push("10"); // Push '10' to the top of the stack.
         Assert.That(rpn.Peek(), Is.EqualTo("10"));
-        rpn.Push("99");
+        rpn.Push("99"); // Push '99' to the top of the stack.
         Assert.That(rpn.Peek(), Is.EqualTo("99"));
-        rpn.Add();
+        rpn.Add(); // 99 + 10 = 109 ('99' is at the top of the stack, followed by '10')
         Assert.That(rpn.Peek(), Is.EqualTo("109"));
     }
 
@@ -28,9 +28,9 @@ public class Tests
     public void VariableTesting()
     {
         using RPN rpn = new();
-        rpn.Parse("50 20 + !temp");
-        rpn.Pop();
-        rpn.Parse("2 @temp *");
+        rpn.Parse("50 20 + !temp"); // 50 + 20 = 70 <-- Store result in temporary variable named 'temp'.
+        rpn.Pop(); // Pops '70' off the stack; which should now be empty.
+        rpn.Parse("2 @temp *"); // Retrieve 'temp' var, which should be '70'. 2 * `temp`(70) = 140.
         Assert.That(rpn.Peek(), Is.EqualTo("140"));
     }
 }
